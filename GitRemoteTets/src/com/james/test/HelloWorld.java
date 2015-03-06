02ef9a11f9e3efca17be1fbc667bf54e43333d81
jian_j <jian_j@235700LT-DFSMS.ad.psb.bls.gov>
2015-03-06 12:43:58 -0500

    add word

---
+++ GitRemoteTets/src/com/james/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.james.test;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word = "Hello Java";
+		System.out.println("Hello world"+word);
+	
+	}
+
+}
